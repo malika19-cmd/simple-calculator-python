87ee29f513baee91d793c6a988893cd46e25f711
malika19-cmd <wadhwamalika50@gmail.com>
2026-01-12 12:25:49 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -44,7 +44,7 @@ def calculator():
 
     num1 = int(input("Enter your first number: "))
 
-    num2 = int(input("Enter your second number: ))
+    num2 = int(input("Enter your second number: "))
                      
     if choice == 1:
       print("Result: ", num1 + num2)
